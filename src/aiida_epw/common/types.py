14cf1fb2e91ff09406e0a555cbe9ec7d61985832
ymzhang0 <zhangyiming1998@gmail.com>
2026-07-07 17:08:41 +0200

    feat(types): add RestartType.NONE and support in EpwCalculation

--- src/aiida_epw/common/types.py
+++ src/aiida_epw/common/types.py
@@ -4,6 +4,7 @@ import enum
 class RestartType(enum.Enum):
     """Enumeration of EPW run/restart modes."""
 
+    NONE = "none"
     EPHWRITE = "ephwrite"
     EPHREAD = "ephread"
     EPHWRITE_RESTART = "ephwrite_restart"
